134d363ce8bae467695ee73240fc4f9a817cfa98
MANOJ KUMAR <Manojgum@gmail.com>
2022-10-14 18:03:54 +0530

    sdjfdlsk

--- src/controllers/get.controller.js
+++ src/controllers/get.controller.js
@@ -3,13 +3,8 @@ const Register = require("../models/user.register.models");
 
 
 
-const getting= async(req,res)=>{
-    try{
-        let user=await Register.find();
-            return res.status(400).send(user)
-    }catch(err){
-        res.status(400).send(err.message)
-    }
+const getting= (req,res)=>{
+    return res.status(400).send("welCome")
 }
 
 
